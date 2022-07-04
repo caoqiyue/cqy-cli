5b6790469345f56d8c23f40953cf675057c042b9
ca1308449030@163.com <ca1308449030@163.com>
2022-07-04 16:46:05 +0800

    优化cqy-cli

--- config.js
+++ config.js
@@ -3,15 +3,4 @@
 module.exports = {
   // 环境变量  debug模式
   log_debug: 'CQY_CLI_DEBUG',
-  // 项目下载的范围
-  project_list: [
-    {
-      value: 'reduxjs/tookit',  // npm项目/git
-      name: 'react-reduxjs/tookit'
-    },
-    {
-      value: 'umi',
-      name: 'react-umi'
-    },
-  ],
 }
